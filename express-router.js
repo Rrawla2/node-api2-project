e4410839bc2559b732d719bf6ab967fc4444aa34
Kristi Gribble <kgribble4083@gmail.com>
2020-03-15 22:15:41 -0500

    Added post and delete requests

--- express-router.js
+++ express-router.js
@@ -2,6 +2,8 @@ const Posts = require("./data/db");
 const express = require("express");
 const router = express.Router();
 
+
+// Get requests
 router.get('/', (req, res) => {
     Posts.find()
         .then(posts => {
@@ -44,5 +46,38 @@ router.get('/:id/comments', (req, res) => {
 })
 
 
+// Post requests
+router.post('/', (req, res) => {
+    const postInfo = req.body
+    console.log(postInfo)
+    Posts.insert(postInfo)
+        .then(post => {
+            if (postInfo.title && postInfo.content) {
+                postInfo.id = shortid.generate();
+                res.status(201).json(postInfo)
+            } else {
+                res.status(400).json({ errorMessage: "Please provide a title and contents for the post." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the post to the database." })
+        })
+})
+
+// Delete request
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Posts.remove(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The post could not be removed." })
+        })
+})
 
 module.exports = router;
